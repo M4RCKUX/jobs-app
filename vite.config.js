a392b9eafd2bda6411f78fa05f739cf6590eb6fe
M4RCKUX <marcossalinas@uoc.edu>
2023-07-25 19:54:07 +0200

    Laravel Breeze Added

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        hmr: {
-            host: 'localhost'
-        }
+      hmr: {
+        host: 'localhost'
+      }
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
